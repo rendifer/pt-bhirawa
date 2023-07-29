f7dadf55ca145f1a02297922ac6899a8ce872b5a
rendifer <rendiferdian2112@gmail.com>
2023-07-29 21:07:56 +0700

    up

--- resources/views/layout/our_solution/product/Building.blade.php
+++ resources/views/layout/our_solution/product/Building.blade.php
@@ -8,7 +8,7 @@
             <a href="/oil and gas" class="dropdown-item">Oil & Gas</a>
             <a href="/Industrial" class="dropdown-item">Industrial</a>
             <a href="/Telecominication" class="dropdown-item">Telecominication</a>
-            <a href="/Building & Infrastucture" class="dropdown-item">Building & Infrastucture</a>
+            <a href="/building& Infrastucture" class="dropdown-item">building& Infrastucture</a>
             <a href="/electrical utility" class="dropdown-item">electrical utility</a>
             <a href="/transportation" class="dropdown-item">transportation</a>
         </ul>
@@ -40,7 +40,7 @@
     <div class="container-fluid page-header wow fadeIn" data-wow-delay="0.1s">
         <div class="container py-5">
             <h4 class="display-6 animated slideInDown mb-4 text-white">
-                Building & Infrastucture
+                building& Infrastucture
             </h4>
         </div>
     </div>
@@ -52,27 +52,11 @@
                 <div class="col-md-4">
                     <div class="owl-carousel testimonial-carousel">
                         <div class="testimonial-item position-relative team-item">
-                            <img class="align-self-center mb-3 py-3 px-3" style="width: 70%; height: 70%;" src="template/img/building /Picture3.png" alt="">
+                            <img class="align-self-center mb-3 py-3 px-3" style="width: 70%; height: 70%;" src="template/img/building/Picture3.png" alt="">
                             <h5 class="text-primary text-center">Marshaling kios</h5>
                         </div>
                         <div class="testimonial-item position-relative team-item">
-                            <img class="align-self-center mb-3 py-3 px-3" style="width: 70%; height: 70%;" src="template/img/building /Picture9.png" alt="">
-                            <h5 class="text-primary text-center">Marshaling kios</h5>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="owl-carousel testimonial-carousel">
-                        <div class="testimonial-item position-relative team-item">
-                            <img class="align-self-center mb-3 py-3 px-3" style="width: 100%; height: 100%;" src="template/img/building /Picture4.png" alt="">
-                            <h5 class="text-primary text-center">Marshaling kios</h5>
-                        </div>
-                        <div class="testimonial-item position-relative team-item">
-                            <img class="align-self-center py-3 px-3 build2" style="width: 100%; height: 100%;" src="template/img/building /Picture5.png" alt="">
-                            <h5 class="text-primary text-center">Marshaling kios</h5>
-                        </div>
-                        <div class="testimonial-item position-relative team-item">
-                            <img class="align-self-center mb-3 py-3 px-3" style="width: 100%; height: 100%;" src="template/img/building /Picture10.png" alt="">
+                            <img class="align-self-center mb-3 py-3 px-3" style="width: 70%; height: 70%;" src="template/img/building/Picture9.png" alt="">
                             <h5 class="text-primary text-center">Marshaling kios</h5>
                         </div>
                     </div>
@@ -80,15 +64,31 @@
                 <div class="col-md-4">
                     <div class="owl-carousel testimonial-carousel">
                         <div class="testimonial-item position-relative team-item">
-                            <img class="align-self-center mb-3 py-3 px-3" style="width: 95%; height: 95%;" src="template/img/building /Picture6.png" alt="">
+                            <img class="align-self-center mb-3 py-3 px-3" style="width: 100%; height: 100%;" src="template/img/building/Picture4.png" alt="">
                             <h5 class="text-primary text-center">Marshaling kios</h5>
                         </div>
                         <div class="testimonial-item position-relative team-item">
-                            <img class="align-self-center mb-3 py-3 px-3" style="width: 95%; height: 95%;" src="template/img/building /Picture8.png" alt="">
+                            <img class="align-self-center py-3 px-3 build2" style="width: 100%; height: 100%;" src="template/img/building/Picture5.png" alt="">
                             <h5 class="text-primary text-center">Marshaling kios</h5>
                         </div>
                         <div class="testimonial-item position-relative team-item">
-                            <img class="align-self-center mb-3 py-3 px-3" style="width: 95%; height: 95%;" src="template/img/building /Picture7.png" alt="">
+                            <img class="align-self-center mb-3 py-3 px-3" style="width: 100%; height: 100%;" src="template/img/building/Picture10.png" alt="">
+                            <h5 class="text-primary text-center">Marshaling kios</h5>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="owl-carousel testimonial-carousel">
+                        <div class="testimonial-item position-relative team-item">
+                            <img class="align-self-center mb-3 py-3 px-3" style="width: 95%; height: 95%;" src="template/img/building/Picture6.png" alt="">
+                            <h5 class="text-primary text-center">Marshaling kios</h5>
+                        </div>
+                        <div class="testimonial-item position-relative team-item">
+                            <img class="align-self-center mb-3 py-3 px-3" style="width: 95%; height: 95%;" src="template/img/building/Picture8.png" alt="">
+                            <h5 class="text-primary text-center">Marshaling kios</h5>
+                        </div>
+                        <div class="testimonial-item position-relative team-item">
+                            <img class="align-self-center mb-3 py-3 px-3" style="width: 95%; height: 95%;" src="template/img/building/Picture7.png" alt="">
                             <h5 class="text-primary text-center">Marshaling kios</h5>
                         </div>
                     </div>
